e33f00530289a6d3f132de0aaecc52a28d5aa7b5
GNeki4 <pnikita43@gmail.com>
2019-12-31 20:58:37 +0500

    added grablee

---
+++ SamosborGame/Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    public float damage;
+    public float range;
+    public float attackCD;
+
+    public Camera fpsCam;
+    private AudioSource attackSound;
+    private float attackTimer;
+
+    private void Start()
+    {
+        attackSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && attackTimer >= attackCD)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                attackSound.Play();
+                target.TakeDamage(damage);
+            }
+        }
+        attackTimer = 0;
+    }
+}
